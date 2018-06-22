a70b081f6473df4777cbb1f080a4d9f776394c6a
secreter <1979510177@qq.com>
2018-06-22 18:52:56 +0800

    :wheelchair: support UMD

--- test/scope.test.js
+++ test/scope.test.js
@@ -1,7 +1,7 @@
 /**
  * Created by pengchaoyang on 2018/5/25
  */
-const parentCodeScoped = require('../lib/scope')
+const scope = require('../lib/scope')
 const expect = require('chai').expect
 let scopeCode = 'parent'
 
@@ -16,7 +16,7 @@ describe('Blank compression', function () {
             color:#444;
             
            }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} h2 { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} h2 { color:#444; }`)
     })
 })
 
@@ -28,33 +28,33 @@ describe('Css normal selector', function () {
         let css = `h2{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} h2 { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} h2 { color:#444; }`)
     })
     it('class should be `parent class`', function () {
         let css = `.title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .title { color:#444; }`)
     })
     it('id should be `parent id`', function () {
         let css = `#title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} #title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} #title { color:#444; }`)
     })
 
     it('tagName.class should be `parent tagName.class`', function () {
         let css = `h2.title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} h2.title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} h2.title { color:#444; }`)
     })
 
     it('* should be `parent *`', function () {
         let css = `*{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} * { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} * { color:#444; }`)
     })
 })
 
@@ -66,25 +66,25 @@ describe('Css sibling and children selector', function () {
         let css = `.header .title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .header .title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .header .title { color:#444; }`)
     })
     it('selectorP > selectorC be `parent selectorP > selectorC`', function () {
         let css = `.header > .title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .header > .title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .header > .title { color:#444; }`)
     })
     it('selectorP + selectorC be `parent selectorP + selectorC`', function () {
         let css = `.header + .title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .header + .title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .header + .title { color:#444; }`)
     })
     it('selectorP ~ selectorC be `parent selectorP ~ selectorC`', function () {
         let css = `.header ~ .title{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .header ~ .title { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .header ~ .title { color:#444; }`)
     })
 })
 
@@ -96,13 +96,13 @@ describe('Css pseudo-class selector', function () {
         let css = `.title:hover{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .title:hover { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .title:hover { color:#444; }`)
     })
     it('selector::pseudo-class should be `selector::pseudo-class`', function () {
         let css = `.title::after{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .title::after { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .title::after { color:#444; }`)
     })
 })
 
@@ -114,13 +114,13 @@ describe('Css attribute selector', function () {
         let css = `.title[href^="https"]{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} .title[href^="https"] { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} .title[href^="https"] { color:#444; }`)
     })
     it('[attribute] should be `parent [attribute]`', function () {
         let css = `[href^="https"]{
             color:#444;
         }`
-        expect(parentCodeScoped(css, scopeCode)).to.be.equal(`.${scopeCode} [href^="https"] { color:#444; }`)
+        expect(scope(css, scopeCode)).to.be.equal(`.${scopeCode} [href^="https"] { color:#444; }`)
     })
 })
 
@@ -134,7 +134,7 @@ describe('Css media query', function () {
                 width: 1450px;
             }
         }`
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`@media (min-width: 1500px) { .${scopeCode} .container { width: 1450px; } }`)
     })
     it('@media() and (){} Css media query`s innerContent should to be `parent selector`', function () {
@@ -146,7 +146,7 @@ describe('Css media query', function () {
                 color:#444;
             }
         }`
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`@media (min-width: 1500px) and (orientation: landscape) { .${scopeCode} .container { width: 1450px; }\n.${scopeCode} a { color:#444; } }`)
     })
 })
@@ -160,7 +160,7 @@ describe('Css keyframes', function () {
             from { margin-left:-20%; }
             to { margin-left:100%; }
         }`
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`@keyframes move_eye { from { margin-left:-20%; }\n to { margin-left:100%; } }`)
     })
 
@@ -174,7 +174,7 @@ describe('Braces have special content', function () {
         let css = `.link-blue{
             color:rgba(0, 0, 255, 0.75);
         }`
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`.${scopeCode} .link-blue { color:rgba(0, 0, 255, 0.75); }`)
     })
 
@@ -186,7 +186,7 @@ describe('Braces have special content', function () {
 describe('Wrong input css', function () {
     it('should be `parent string`', function () {
         let css = `.link-blue`
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`.${scopeCode} .link-blue`)
     })
 
@@ -195,7 +195,7 @@ describe('Wrong input css', function () {
             color:rgba(0, 0, 255, 0.75);
         }`
         scopeCode='9527'
-        expect(parentCodeScoped(css, scopeCode))
+        expect(scope(css, scopeCode))
             .to.be.equal(`._${scopeCode} .link-blue { color:rgba(0, 0, 255, 0.75); }`)
     })
 
